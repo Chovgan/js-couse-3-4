8e46657c41dafe46b72d527536513467b610477f
Khrystyna Chovgan <khrystyna.chovgan@quandoo.com>
2019-01-17 01:00:53 +0100

    Progress DZ 2

--- 01-intro-js/test/bank-account.test.js
+++ 01-intro-js/test/bank-account.test.js
@@ -4,24 +4,28 @@ var getNewBankAccount = require('../bank-account');
 describe('Bank account', function () {
     it('should be able to deposit money on bank account', function () {
         var BankAccount = getNewBankAccount();
+        return this.BankAccount + 100;
         // BankAccount.deposit
         assert.equal(BankAccount.balance, 100);
     });
 
     it('should be able to widthaw money from bank account', function () {
         var BankAccount = getNewBankAccount();
+        return this.BankAccount - 50;
         // BankAccount.widthraw
         assert.equal(BankAccount.balance, 50);
     });
 
     it('should be able to not let get more money from bank account that user has', function () {
         var BankAccount = getNewBankAccount();
+        return BankAccount.balance;
         // BankAccount.widthraw
         assert.equal(BankAccount.balance, 100);
     });
 
     it('should be able to get more money from bank account that user has credit limit', function () {
         var BankAccount = getNewBankAccount();
+        return BankAccount.balance - 200;
         // BankAccount.widthraw
         assert.equal(BankAccount.balance, -200);
     });
