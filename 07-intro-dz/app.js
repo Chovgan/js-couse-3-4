1d9a553f4a622d30e5313f3c42085c8f552bef03
Khrystyna Chovgan <khrystyna.chovgan@quandoo.com>
2019-02-04 13:35:54 +0100

    WIP

---
+++ 07-intro-dz/app.js
@@ -0,0 +1,10 @@
+(function(){
+
+    var data = {
+        image: './fjords.jpg',
+    }
+
+    var dogoImageElement = document.getElementById('fjords-image');
+
+    dogoImageElement.src = data.image;
+})();
